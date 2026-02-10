4c591afd0c0566c875b840e8de1c0b0d2045538d
artem10001 <artem.yakunin@edu.keuda.fi>
2026-02-10 09:06:15 +0200

    juu tässä koodailin pikkusen

---
+++ demo6/kirjautuminen.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", onkoKirjautunut);
+function onkoKirjautunut(){
+    let kirjautunut = "kylla";
+    if(kirjautunutu === "kylla") {
+        document.getElementByld("tervetuloa_teksti").textContent += " Otto!";
+    }
+}
